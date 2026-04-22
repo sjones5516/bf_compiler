fdd052c684b0872666161c5269c4185d041fac5a
Samuel Jones <sjones.nova@gmail.com>
2026-04-22 14:30:01 -0400

    Created enum to represent BF tokens

--- src/include/token.hpp
+++ src/include/token.hpp
@@ -1,4 +1,20 @@
 #ifndef TOKEN_HPP
 #define TOKEN_HPP
 
+namespace bf {
+enum class Token {
+  /*
+   * Represents all executable tokens in a BF program.
+   */
+  kIncrementDataPointer,
+  kDecrementDataPointer,
+  kIncrementByte,
+  kDecrementByte,
+  kOutputByte,
+  kAcceptByte,
+  kStartLoop,
+  kEndLoop
+};
+}
+
 #endif
